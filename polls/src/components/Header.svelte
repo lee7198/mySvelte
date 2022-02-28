36c0db31cb01f1eca505b41158189bc8d6dcbb5e
lee7198 <lee7198@naver.com>
2022-02-28 21:08:35 +0900

    master

---
+++ polls/src/components/Header.svelte
@@ -0,0 +1,19 @@
+<header>
+  <h1>
+    <img src="/img/poll_logo.png" alt="Poll Logo">
+  </h1>
+</header>
+
+<style>
+  header {
+    background: #f7f7f7;
+    padding: 20px;
+  }
+  h1 {
+    margin: 0;
+    text-align: center;
+  }
+  img {
+    max-width: 80px;
+  }
+</style>
---
+++ polls/src/shared/Tabs.svelte
@@ -0,0 +1,41 @@
+<script>
+  import {createEventDispatcher } from 'svelte';
+  const dispatch = createEventDispatcher();
+
+  export let items;
+  export let activeItem;
+
+</script>
+
+<div class="tabs">
+  <ul>
+    {#each items as item}
+      <li on:click={() => dispatch('tabChange', item)}>
+        <div class:active={item === activeItem}>{item}</div>
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  ul {
+    display: flex;
+    justify-content: center;
+    padding: 0;
+    list-style: none;
+  }
+  li {
+    margin: 0 16px 0;
+    font-size:18px;
+    color: #555;
+    cursor: pointer;
+  }
+  .tabs {
+    margin-bottom: 40px;
+  }
+  .active {
+    color:cadetblue;
+    border-bottom: 2px solid cadetblue;
+    padding-bottom: 8px;
+  }
+</style>
